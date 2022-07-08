f6b226f023a6b5e02b45fffc065c984e5b159d14
Alonso Anchante Moreno <alonso.amoreno@live.com>
2022-07-07 21:53:14 -0500

    Ultimo Commit

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -7,18 +7,16 @@ namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+
         private readonly IRepositorioProyectos repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        public HomeController(IRepositorioProyectos repositorioProyectos)
         {
-            _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
         }
 
         public IActionResult Index()
         {
-            var repositorioProyectos = new RepositorioProyectos();
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel()
             {
@@ -27,7 +25,25 @@ namespace Portafolio.Controllers
             return View(modelo);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
+
+        [HttpGet]
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
         {
             return View();
         }
